e9fd66abc939d2059ab47ebfecd72cbf25794be4
github username <rachel.salois@gmail.com>
2017-06-03 10:04:50 -0400

    added mail_to instead of action mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def authenticate_user
+    redirect_to login_path unless logged_in?
+  end
 end
