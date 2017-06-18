a7e6d09684f0525eafa5c2f5b186d8320f720de5
github username <rachel.salois@gmail.com>
2017-06-18 17:33:34 -0400

    blog CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :users
   resources :program_classrooms
   resources :collaborations
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
