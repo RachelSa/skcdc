8b7d899d2e5c5616b410d81d6264b1dedccc8294
github username <rachel.salois@gmail.com>
2017-09-02 07:12:59 -0400

    post date styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822111554) do
+ActiveRecord::Schema.define(version: 20170831110733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20170822111554) do
     t.string   "upload_attachment_content_type"
     t.integer  "upload_attachment_file_size"
     t.datetime "upload_attachment_updated_at"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
   create_table "program_classrooms", force: :cascade do |t|
