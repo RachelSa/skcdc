ddd8f69af9cb992ab17cf6db77670937eccfff5c
github username <rachel.salois@gmail.com>
2017-09-02 15:49:20 -0400

    fixed jquery

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,4 +1,8 @@
-$(document).ready(function() {
-  $(".dropdown-button").dropdown({ hover: true, constrainWidth: false });
-  $(".button-collapse").sideNav();
+$(document).on('turbolinks:load', function() {
+  $(".dropdown-button").dropdown({
+    hover: true,
+  });
+  $(".button-collapse").sideNav({
+    edge: 'right',
+    constrainWidth: false });
 });
