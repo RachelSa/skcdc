819b5a2a393a1944d4a943ac395e2065b21d8a00
github username <rachel.salois@gmail.com>
2017-09-17 12:15:21 -0400

    fixed login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,13 @@ class SessionsController < ApplicationController
   def create
    if !User.find_by(email: params[:user][:email].downcase).try(:authenticate, params[:user][:password])
      flash[:notice] = "invalid login credentials"
+     @admin = Contact.find_by(title: "Information Systems Manager")
      render :new
    else
      @user = User.find_by(email: params[:user][:email].downcase)
      session[:user_id] = @user.id
      flash[:notice] = "successful login"
-     redirect_to users_path
+     redirect_to admin_path
    end
  end
 
