2728330c238415f9c256e3937381203d013044fe
github username <rachel.salois@gmail.com>
2017-09-21 19:44:48 -0400

    created partial

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def url_classname
+    self.class.to_s.split("::").first.underscore.pluralize
+  end
+
+  def self.title_classname
+    self.name.underscore.split("_").join(" ").pluralize
+  end
+
+  def title_or_name
+    if self.title
+      self.title
+    else
+      self.name
+    end
+  end
 end
