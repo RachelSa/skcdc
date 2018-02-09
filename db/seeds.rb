b86e634a03fb3dd94557f84caf7c56c0e03002fb
github username <rachel.salois@gmail.com>
2018-02-09 13:09:57 -0500

    finished gem etc updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 require 'csv'
 
-# collab_csv = File.read("./db/collaborations.csv")
-# collabs = CSV.parse(collab_csv)
-# collabs.each do |row|
-#   Collaboration.create(title: row[0], description:row[1])
-# end
+collab_csv = File.read("./db/collaborations.csv")
+collabs = CSV.parse(collab_csv)
+collabs.each do |row|
+  Collaboration.create(title: row[0], description:row[1])
+end
 
-# contacts_csv = File.read("./db/contacts.csv")
-# collabs = CSV.parse(contacts_csv)
-# collabs.each do |row|
-#   Contact.create(first_name:row[0], last_name:row[1], title:row[2], ext:row[3], email:row[4])
-# end
+contacts_csv = File.read("./db/contacts.csv")
+collabs = CSV.parse(contacts_csv)
+collabs.each do |row|
+  Contact.create(first_name:row[0], last_name:row[1], title:row[2], ext:row[3], email:row[4])
+end
 
-# locations_csv = File.read("./db/locations.csv")
-# locations = CSV.parse(locations_csv)
-# locations.each do |loc|
-#   Classroom.create(name:loc[0], street_address:loc[1], town:loc[2], zipcode:loc[3], phone:loc[4], description:"add description from spreadsheet")
-# end
+locations_csv = File.read("./db/locations.csv")
+locations = CSV.parse(locations_csv)
+locations.each do |loc|
+  Classroom.create(name:loc[0], street_address:loc[1], town:loc[2], zipcode:loc[3], phone:loc[4], description:"description to come!")
+end
 
-# c = Carousel.create()
+c = Carousel.create()
