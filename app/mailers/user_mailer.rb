b5f77da5937c8913e63ee52d62b6393e98d14ecc
github username <rachel.salois@gmail.com>
2018-02-09 13:30:02 -0500

    syntax

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,4 @@
 class UserMailer < ApplicationMailer
 
-  def request_login(user)
-   @site_administrator = Contact.find_by(title: "Information Systems Manager")
-   @director = Contact.find_by(title: "Agency Director")
-   @url  = 'http://skcdc.org/users/new'
-   mail(to: "rachel.salois@gmail.com", subject: 'Welcome to My Awesome Site')
- end
-end
+
 end
